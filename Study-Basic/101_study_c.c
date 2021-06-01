e937129466ebd15272c23df76d3b8a459e62a51d
WeMayNeverKnow <77531972+WeMayNeverKnow@users.noreply.github.com>
2021-06-01 21:36:09 +0800

    nani

---
+++ Study-Basic/101_study_c.c
@@ -0,0 +1,4 @@
+main() /*function main is always the first thing called in C*/
+{ /*The things covers in curly brackets are the body of the function of in this case main*/
+  printf("Hello you owo"); /*to print strings we use `"` instead of `'` which is for the ascii'*/
+}
